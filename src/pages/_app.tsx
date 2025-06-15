0b35293195c0445f644a06d5aed0213cb7dbb1d3
Petr Homoky <p.homoky@gmail.com>
2025-06-15 16:10:36 +0200

    [code] add Vercel Analytics integration for user tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
