17e9dad5f268e3b4a5155586e6b55ac9c7f4272f
Petr Homoky <p.homoky@gmail.com>
2025-06-23 19:45:00 +0200

    [code] add new update section to AboutMe component with email feedback acknowledgment

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -44,7 +44,13 @@ export const AboutMe = () => (
           . I'd love to help take Volvo's infotainment system to the next level!
         </p>
         <div className="pt-4">
-          <Tag className="bg-[#FFD429] text-black">Update 2025/06/19</Tag>
+          <Tag className="bg-[#FFD429] text-black">Update 2025/06/23</Tag>
+          <p className="mt-4">
+            Thank you so much for all the emails with feedback and suggestions! I'm gradually implementing those that align with my criteria for bugs or improvements. Some feedback relates to the twin motor variant or Ultra trim level that I don't currently have - I'll incorporate those once my new car arrives, which I expect around July 11th for delivery.
+          </p>
+        </div>
+        <div className="pt-4">
+          <Tag className="bg-gray-200 text-gray-700">Update 2025/06/19</Tag>
           <p className="mt-4">
             This project is gaining real traction! Several people from Volvo
             Cars have reached out privately, asking me to add tags to help them
